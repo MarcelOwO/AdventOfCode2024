dae72041f8a38a08042c57b38a9a8d98558d1346
Marcel Sigl <marcel-sigl@gmx.de>
2024-12-05 14:20:03 +0100

    day 5

--- AOC.Console/Program.cs
+++ AOC.Console/Program.cs
@@ -3,14 +3,19 @@
 Console.WriteLine("Starting");
 
 var solver = new SolverController();
-List<Task> tasks = new();
 
+await solver.SolveAsync(5);
+
+
+/*
+    List<Task> tasks = new();
 Enumerable.Range(1, 4).ToList().ForEach(x =>
 {
-    var task =  solver.SolveAsync(x); 
+    var task =  solver.SolveAsync(x);
     tasks.Add(task);
 });
 
 Task.WaitAll(tasks.ToArray());
+*/
 
 Console.WriteLine("Finished");
--- AOC.Solvers/Solvers/Day5Solver.cs
+++ AOC.Solvers/Solvers/Day5Solver.cs
@@ -1,19 +1,100 @@
+using System.Collections.Concurrent;
 using AOC.Solvers.Interfaces;
 
-public class Day5Solver: ISolver
+public class Day5Solver : ISolver
 {
-    public Task<string> SolveAsync(StreamReader input)
+    private List<List<int>> Lists { get; set; } = [];
+    private ConcurrentDictionary<int, List<int>> RuleDict { get; set; } = [];
+
+    public async Task<string> SolvePart1Async(StreamReader input)
     {
-        throw new NotImplementedException();
+        bool switchFlag = false;
+
+        while (!input.EndOfStream)
+        {
+            var line = await input.ReadLineAsync();
+
+            if (string.IsNullOrWhiteSpace(line) && !switchFlag)
+            {
+                switchFlag = true;
+                continue;
+            }
+
+            if (switchFlag)
+            {
+                Lists.Add(line.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+            }
+            else
+            {
+                var parts = line.Split('|').Select(int.Parse).ToList();
+
+                if (!RuleDict.TryAdd(parts[0], [parts[1]]))
+                {
+                    RuleDict[parts[0]].Add(parts[1]);
+                }
+            }
+        }
+
+        var values = new ConcurrentBag<int>();
+
+        Parallel.ForEach(Lists, async list =>
+        {
+            var check = list.All(element =>
+            {
+                var list2 = Enumerable.Range(0, list.IndexOf(element)).Select(x => list[x]).ToList();
+                return RuleDict[element].ToList().All(x => !list2.Contains(x));
+            });
+
+            if (check)
+            {
+                values.Add(list[list.Count / 2]);
+            }
+        });
+
+        return values.Sum().ToString();
     }
 
-    public Task<string> SolvePart1Async(StreamReader input)
+    public async Task<string> SolvePart2Async(StreamReader input)
     {
-        throw new NotImplementedException();
-    }
+        var values = new ConcurrentBag<int>();
 
-    public Task<string> SolvePart2Async(StreamReader input)
-    {
-        throw new NotImplementedException();
+        Parallel.ForEach(Lists, list =>
+        {
+            var check = list.All(element =>
+            {
+                var list2 = Enumerable.Range(0, list.IndexOf(element)).Select(x => list[x]).ToList();
+                return RuleDict[element].ToList().All(x => !list2.Contains(x));
+            });
+
+            if (check)
+            {
+                return;
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    if (RuleDict[list[j]].Contains(list[i]))
+                    {
+                        list[i] = list[j] ^ list[i];
+                        list[j] = list[i] ^ list[j];
+                        list[i] = list[j] ^ list[i];
+
+                        i = 0;
+                        j = -1;
+                    }
+                }
+            }
+
+            values.Add(list[list.Count / 2]);
+        });
+
+        return values.Sum().ToString();
     }
 }
